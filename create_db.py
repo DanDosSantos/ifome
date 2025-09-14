1341b1148e9da563de9107af3dcfb24e503c17ab
admin <danpaarker@gmail.com>
2025-09-14 17:37:34 -0300

    feature: criação da tabela de restaurantes

--- create_db.py
+++ create_db.py
@@ -4,6 +4,7 @@ from config_db import db
 # Importa todos os models para o SQLAlchemy "conhecer"
 from src.models.usuarios_model import Usuarios
 from src.models.endereco_model import Endereco
+from src.models.restaurante_model import Restaurante
 app = create_app()
 
 with app.app_context():
